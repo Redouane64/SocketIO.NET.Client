ce0fb2751c56274bf1500a8d5f9a75e4b1aea12f
redouane <redouane.sobaihi@live.com>
2024-02-26 21:57:41 +0100

    feat: add documentation comments

--- src/EngineIO.Client/Packet.cs
+++ src/EngineIO.Client/Packet.cs
@@ -2,6 +2,9 @@ using System.Text;
 
 namespace EngineIO.Client;
 
+/// <summary>
+///     Represent EIO protocol packet types. see: https://socket.io/docs/v4/engine-io-protocol/#protocol
+/// </summary>
 public enum PacketType : byte
 {
     Open = 0x30,
@@ -15,33 +18,59 @@ public enum PacketType : byte
 
 public struct Packet
 {
-    public static Packet Pong()
+    private static readonly Packet _pongPacket = new([(byte)PacketType.Pong]);
+    private static readonly Packet _closePacket = new([(byte)PacketType.Close]);
+
+    /// <summary>
+    ///     Create Pong packet.
+    /// </summary>
+    /// <returns>Packet of type Pong</returns>
+    public static Packet PongPacket()
     {
-        return new Packet([(byte)PacketType.Pong]);
-    }
-    
-    public static Packet ClosePacket()
-    {
-        return new Packet([(byte)PacketType.Close]);
+        return _pongPacket;
     }
 
-    private string? _text;
+    /// <summary>
+    ///     Create Close packet.
+    /// </summary>
+    /// <returns>Packet of type Close</returns>
+    public static Packet ClosePacket()
+    {
+        return _closePacket;
+    }
+
+    private string _text;
+
+    /// <summary>
+    ///     Decode packet payload using Encoding.UTF8.
+    /// </summary>
+    public string? Text()
+    {
+        if (Payload.Length > 0) return null;
+
+        if (_text is not null) return _text;
+
+        _text = Encoding.UTF8.GetString(Payload);
+        return _text;
+    }
 
     public Packet(byte[] data)
     {
         Data = data;
     }
 
+    /// <summary>
+    ///     Packet raw payload, including packet type flag.
+    /// </summary>
     public byte[] Data { get; }
 
+    /// <summary>
+    ///     Packet type.
+    /// </summary>
     public PacketType Type => (PacketType)Data[0];
+
+    /// <summary>
+    ///     Packet payload without type flag.
+    /// </summary>
     public byte[] Payload => Data.AsSpan(1..).ToArray();
-
-    public override string ToString()
-    {
-        if (_text is not null) return _text;
-
-        _text = Encoding.UTF8.GetString(Payload);
-        return _text;
-    }
 }

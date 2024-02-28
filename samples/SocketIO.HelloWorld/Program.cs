0eb5b116a598f51fc779cd31885e3b39dec619d1
redouane <redouane.sobaihi@live.com>
2024-02-28 12:49:45 +0100

    feat: update logger

--- samples/SocketIO.HelloWorld/Program.cs
+++ samples/SocketIO.HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using EngineIO.Client;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace SocketIO.Client.HelloWorld;
 
@@ -9,15 +10,18 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var logger = LoggerFactory.Create(builder => { builder.AddConsole().SetMinimumLevel(LogLevel.Debug); })
-            .CreateLogger<Engine>();
+        var logger = LoggerFactory.Create(builder => {
+                builder.AddSimpleConsole(o => {
+                        o.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] ";
+                        o.SingleLine = true;
+                        o.ColorBehavior = LoggerColorBehavior.Enabled;
+                }).SetMinimumLevel(LogLevel.Debug);
+            });
 
-        using (var engine = new Engine("http://127.0.0.1:9854", logger))
-        {
-            await engine.ConnectAsync();
+        using var engine = new Engine("http://127.0.0.1:9854", logger);
+        await engine.ConnectAsync();
 
-            Console.ReadKey();
-            await engine.DisconnectAsync();
-        }
+        Console.ReadKey();
+        await engine.DisconnectAsync();
     }
 }

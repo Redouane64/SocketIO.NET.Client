ad5898048a5f6ed1d278b977923d7ce123e50bb4
redouane <redouane.sobaihi@live.com>
2024-04-17 22:35:50 +0100

    test(websocket): add websocket tests

---
+++ tests/EngineIO.Client.Tests/Transports/WebSocketTransportTests.cs
@@ -0,0 +1,17 @@
+using System.Net.WebSockets;
+using EngineIO.Client.Transports;
+
+namespace EngineIO.Client.Tests.Transports;
+
+public sealed class WebSocketTransportTests
+{
+    [Fact]
+    async void Should_Create_Transport()
+    {
+        var baseAddress = "http://example.com";
+        var sid = "1NkM2QzZGMjEyMTIxCg";
+        var client = new ClientWebSocket();
+        var transport = new WebSocketTransport(baseAddress, sid, client);
+        Assert.False(transport.Connected);    
+    }
+}

25ab855320f5fb3ca49b289aa8ded1b35d28fe10
redouane <redouane.sobaihi@live.com>
2024-04-18 01:22:55 +0100

    fix: update exception type implementation
    - make `BadRequestError.Message` nullable
    - remove duplicate `Message` property

--- src/EngineIO.Client/Transports/Exceptions/BadRequestException.cs
+++ src/EngineIO.Client/Transports/Exceptions/BadRequestException.cs
@@ -9,18 +9,16 @@ internal class BadRequestError
     public int Code { get; set; }
 
     [JsonPropertyName("message")]
-    public string Message { get; set; }
+    public string? Message { get; set; }
 }
 
 public class BadRequestException : Exception
 {
     public int Code { get; }
-    public string Message { get; }
-    
+
     public BadRequestException(int code, string message)
         : base(message)
     {
         Code = code;
-        Message = message;
     }
 }

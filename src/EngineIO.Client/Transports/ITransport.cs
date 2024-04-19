ba232602da36698579d1aa7b310074f03b956de2
redouane <redouane.sobaihi@live.com>
2024-04-19 22:09:35 +0100

    feat(transport): add documentation

--- src/EngineIO.Client/Transports/ITransport.cs
+++ src/EngineIO.Client/Transports/ITransport.cs
@@ -9,29 +9,35 @@ namespace EngineIO.Client.Transports;
 public interface ITransport
 {
     /// <summary>
-    ///     Transport name.
+    /// Transport name.
     /// </summary>
     string Name { get; }
 
     /// <summary>
-    ///     Perform transport handshake.
+    /// Perform transport handshake.
     /// </summary>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     Task ConnectAsync(CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Set transport state to closed.
+    /// </summary>
     void Close();
 
+    /// <summary>
+    /// Disconnect transport by sending close packet to remote server.
+    /// </summary>
     Task Disconnect();
 
     /// <summary>
-    ///     Fetch raw packets from server.
+    /// Fetch raw packets from server.
     /// </summary>
     /// <returns>Packets as an array of Bytes.</returns>
     Task<ReadOnlyCollection<ReadOnlyMemory<byte>>> GetAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
-    ///     Send raw packets to server.
+    /// Send raw packets to server.
     /// </summary>
     /// <param name="packets"></param>
     /// <param name="format"></param>

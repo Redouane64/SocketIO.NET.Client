63f0ccebf5ac6b1a5e6f5050568db5a13b8f3fe4
redouane <redouane.sobaihi@live.com>
2024-08-25 16:05:26 +0100

    feat: prototype client API surface

--- src/SocketIO.Client/IO.cs
+++ src/SocketIO.Client/IO.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 using EngineIO.Client;
 
@@ -8,12 +11,47 @@ namespace SocketIO.Client;
 public class IO
 {
     private readonly Engine _client;
+    private readonly string _baseUrl;
     
     // Map namespace with its corresponding sid
     private readonly Dictionary<string, string> _namespaces = new();
 
-    public IO()
+    public IO(string baseUrl)
     {
-        
+        this._baseUrl = baseUrl;
+
+        this._client = new Engine((config) =>
+        {
+            config.BaseAddress = baseUrl;
+            config.AutoUpgrade = true;
+            
+            // TODO: allow passing custom headers and queries
+        });
+    }
+
+    public Task Connect(string? @namespace = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async IAsyncEnumerable<ReadOnlyMemory<byte>> ListenAsync(
+        string? @namespace = default, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        yield return Array.Empty<byte>();
+    }
+
+    public Task SendAsync(string? @namespace, ReadOnlyMemory<byte> data)
+    {
+        return Task.CompletedTask;
+    }
+    
+    public Task SendAsync<T>(string? @namespace, T data)
+    {
+        return Task.CompletedTask;
+    }
+    
+    public Task SendAsync<T>(string? @namespace, T[] data)
+    {
+        return Task.CompletedTask;
     }
 }
